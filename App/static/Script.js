14620b302fa91f154066d775a0728738136df61e
Oskar Przybylski <oskar.przybylski23@gmail.com>
2023-12-04 15:20:58 +0000

    change folder structure and fetch URL

--- App/static/Script.js
+++ App/static/Script.js
@@ -1,13 +1,13 @@
 function getCollection() {
-  fetch("/get_collection")
+  fetch('http://127.0.0.1:5000/get_collection')
     .then((response) => response.json())
     .then((data) => {
       // Update the UI with the data
-      const albumList = document.getElementById("album-list");
-      albumList.innerHTML = ""; // Clear previous data
+      const albumList = document.getElementById('album-list');
+      albumList.innerHTML = ''; // Clear previous data
 
       data.forEach((album) => {
-        const listItem = document.createElement("li");
+        const listItem = document.createElement('li');
         listItem.textContent = `${album.artist} - ${album.title}`;
         albumList.appendChild(listItem);
       });
